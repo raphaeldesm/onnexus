7034db846dfb26b66c0a17525b8c762fae4ccaf0
Raphael de Souza Marinho <raphaeldesm@hotmail.com>
2025-08-26 11:46:55 -0400

    Adicionar projeto

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from courses.models import Course
 
 def home(request):
-    posts = Post.objects.all()
-    courses = Course.objects.all()
+    posts = Post.objects.filter().order_by('-id')[:4]
+    courses = Course.objects.filter(title__icontains='Manipular Imagens com Photoshop')
 
     return render(request, 'home/home-base.html', {'posts': posts, 'courses': courses})
