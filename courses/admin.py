fa9502c302305ba739ec5f6027abc7505dde9697
Raphael de Souza Marinho <raphaeldesm@hotmail.com>
2025-09-02 08:59:48 -0400

    Atualizar projeto

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Instructor, Course
 
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'instructor', 'is_buyable')
+    list_filter = ('is_buyable', 'instructor')
+    search_fields = ('title', 'description')
+
 admin.site.register(Instructor)
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
--- courses/models.py
+++ courses/models.py
@@ -12,6 +12,7 @@ class Course(models.Model):
     description = models.TextField()
     video_thumbnail = models.ImageField(upload_to='thumbnails/')
     instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE, related_name='courses')
+    is_buyable = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
